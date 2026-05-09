792d1227fddb7037c08111c84b706f881a0b7cf3
Patrick Jimison <pat.jimison@gmail.com>
2026-05-09 07:58:54 -0700

    Create PublishJSON for sending data to a channel

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"os"
+	"os/signal"
+
+	"github.com/pjjimiso/learn-pub-sub-starter/internal/pubsub"
+	"github.com/pjjimiso/learn-pub-sub-starter/internal/routing"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	connectionString := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connectionString)
+	if err != nil {
+		fmt.Println("Failed to create rabbitMQ connection:", err)
+	}
+	defer conn.Close()
+	fmt.Println("Connection successful!")
+
+	// Open channel
+	channel, err := conn.Channel()
+	if err != nil {
+		fmt.Println("Failed to open a new channel:", err)
+
+	}
+
+	pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+
+	// Wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Shutting down...")
+	conn.Close()
 }
